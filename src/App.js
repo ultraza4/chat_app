cadd2c9b492d9cd9ca4b41b588e9080bad83e8a8
Azamat Work <you@example.com>
2022-10-26 14:41:52 +0600

    home page with chats

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import Register from "./pages/Register";
+import Login from "./pages/Login";
 import "./style.scss";
+import Home from "./pages/Home";
 
 function App() {
   return (
     <div className="App">
-      <Register />
+      <Home />
     </div>
   );
 }
--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -2,7 +2,18 @@ import React from 'react'
 
 const Login = () => {
   return (
-    <div>Login</div>
+    <div className='formContainer'>
+        <div className="formWrapper">
+            <span className='logo'>Ozen Chat</span>
+            <span className='title'>Sign In</span>
+            <form>
+                <input type="email" placeholder='Email'/>
+                <input type="password" placeholder='Password'/>
+                <button>Sign In</button>
+            </form>
+            <p>You don't have an account? Register</p>
+        </div>
+    </div>
   )
 }
 
