ba2f1339f806e9c458d3d30e5166c814f292ec4c
Azamat Work <you@example.com>
2022-11-03 15:45:32 +0600

    messages component

--- src/components/Message.jsx
+++ src/components/Message.jsx
@@ -2,7 +2,15 @@ import React from 'react'
 
 const Message = () => {
   return (
-    <div>Message</div>
+    <div className='message'>
+      <div className="messageInfo">
+        <img src="https://images.pexels.com/photos/13996231/pexels-photo-13996231.jpeg?cs=srgb&dl=pexels-manuel-michael-13996231.jpg&fm=jpg&_gl=1*2vm57e*_ga*MTAxMDExMzAyOS4xNjU1MzU0MDQz*_ga_8JE65Q40S6*MTY2NzQ2MzQyMy44LjEuMTY2NzQ2MzQyNS4wLjAuMA.." alt="" />
+        <span>Just now</span>
+      </div>
+      <div className="messageContent">
+        <p>Hello</p>
+      </div>
+    </div>
   )
 }
 
