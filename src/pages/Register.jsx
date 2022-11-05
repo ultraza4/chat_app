1fb19ce9d891c619b28f4cd9bee55c96bb6e7e33
Azamat Work <you@example.com>
2022-11-05 11:08:57 +0600

    download Avatar function and created firestore for users

--- src/pages/Register.jsx
+++ src/pages/Register.jsx
@@ -1,31 +1,50 @@
 import React from 'react';
-import { createUserWithEmailAndPassword } from "firebase/auth";
-import { auth } from "../firebase";
+import addAvatar from '../images/addAvatar.png'
+import { createUserWithEmailAndPassword, updateProfile } from "firebase/auth";
+import { auth, storage, db } from "../firebase";
+import { useState } from 'react';
+import { ref, uploadBytesResumable, getDownloadURL } from "firebase/storage";
+import { doc, setDoc } from "firebase/firestore";
 
 const Register = () => {
 
-  const handleSubmit = (e) => {
-    e.preventDefault()
+  const [err, setErr] = useState();
 
+  const handleSubmit = async (e) => {
+    e.preventDefault()
     const displayName = e.target[0].value;
     const email = e.target[1].value;
     const password = e.target[2].value;
     const file = e.target[3].value[0];
 
-    createUserWithEmailAndPassword(auth, email, password)
-      .then((userCredential) => {
-        // Signed in 
-        const user = userCredential.user;
-        console.log(user)
-      })
-      .catch((error) => {
-        const errorCode = error.code;
-        const errorMessage = error.message;
-        // ..
+    try {
+      const res = await createUserWithEmailAndPassword(auth, email, password)
+      const date = new Date().getTime();
+      const storageRef = ref(storage, `${displayName + date}`);
+
+      await uploadBytesResumable(storageRef, file).then(() => {
+        getDownloadURL(storageRef).then(async (downloadURL) => {
+          try {
+            await updateProfile(res.user, {
+              displayName,
+              photoURL: downloadURL
+            })
+
+            await setDoc(doc(db, "users", res.user.uid), {
+              uid: res.user.uid,
+              displayName,
+              email,
+              photoURL: downloadURL
+            })
+          } catch (error) {
+            setErr(true)
+          }
+        });
       });
-
+    } catch (error) {
+      setErr(true)
+    }
   }
-
   return (
     <div className='formContainer'>
       <div className="formWrapper">
@@ -35,11 +54,13 @@ const Register = () => {
           <input type="text" placeholder='Display Name' />
           <input type="email" placeholder='Email' />
           <input type="password" placeholder='Password' />
-          <input style={{ display: "none" }} type="file" id='file' />
+          <input style={{ display: 'none' }} type="file" id='file' />
           <label htmlFor="file">
-            <img src="" alt="" />
+            <img src={addAvatar} alt="" />
+            <span>Add an avatar</span>
           </label>
           <button>Sign Up</button>
+          {err && <span>Something went wrong</span>}
         </form>
         <p>You do have an account? Login</p>
       </div>
